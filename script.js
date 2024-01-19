81204a17de435936655c0d741f6a3989c7955c1d
rfloarea <account@rfloarea.net>
2024-01-19 15:12:57 -0500

    clean up cruft

--- script.js
+++ script.js
@@ -1,56 +1,21 @@
 const gridFrame = document.getElementById('gridFrame');
 const box = document.createElement('div')
 
-
-
 // create an iterable array of i length
 i = prompt();
 let arr = Array.from('x'.repeat(i));
 console.log(arr.length);
 
-// append a new box element to the parent container element
+// append a new box element to the parent container every time we iterate over the array
 function appendBox(j) {
     for (j = 0; j < arr.length; j++ ){
-        // checking
-        console.log(arr);
         // create a new element per item in array
         const newElement  = document.createElement('div');
-        // add the box class to element i
+        // add the box class to the new element
         newElement.classList.add('box');
-        // append i to parent element
+        // append the new element to parent element
         gridFrame.appendChild(newElement);
     }
 }
 
 appendBox();
-
-
-
-// so now we need to figure out how to create i number of box elements and append them i number of times
-
-
-// i = 3
-// let arr = Array.from('x'.repeat(i))
-// console.log(arr)
-// //logs ['x', 'x', 'x']
-
-// i = 3
-// let arr = (Array.from('x'.repeat(i)))
-// arr.forEach((element) => console.log(element))
-// // x
-// // x
-// // x
-
-// i = 3
-// let arr = Array.from('x'.repeat(i)).forEach((element) => console.log(element))
-// // x
-// // x
-// // x
-// // 3
-
-// i = prompt();
-// let arr = Array.from('x'.repeat(i)).forEach((element) => console.log(element));
-// // input is printed input times
-
-// rather than forEach calling element, why not call a function that inserts a new node?
-
