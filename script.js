5a7cc291e97488c4bca9c9641b27d9bff9303967
rfloarea <account@rfloarea.net>
2024-01-22 15:38:11 -0500

    iterate over the NodeList of new elements to change their color

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 const gridFrame = document.getElementById('gridFrame');
 const box = document.createElement('div')
 
-// create an iterable array of length i
-i = prompt('How many boxes would you like in your canvas? Max is 256.');
-let arr = Array.from('x'.repeat(i));
-// console.log(arr.length);
+numberOfBoxes = prompt('How many boxes would you like in your canvas? Max is 256.');
+let arr = Array.from('x'.repeat(numberOfBoxes));
 
-// append a new box element to the parent container every time we iterate over the array
-function appendBox(j) {
-    for (j = 0; j < arr.length; j++ ){
-        // create a new element per item in array
+function appendBox(i) {
+    for (i = 0; i < arr.length; i++ ){
         const newElement  = document.createElement('div');
-        // add the box class to the new element
         newElement.classList.add('box');
-        // append the new element to parent element
+        newElement.setAttribute('id', 'box');
         gridFrame.appendChild(newElement);
     }
 }
 
 appendBox();
+
+
+
+const newBoxList = document.querySelectorAll('#box');
+console.log(newBoxList); // this logs a NodeList of all 256 elements
+
+/* newBox is a NodeList (an array type object) of all elements with id = box
+ok...so now what?
+now we need to find a way to attach event listeners to each node on this list
+is that possible?
+let's see
+*/
+
+for (let i = 0; i < newBoxList.length; i++) {
+    function changeBoxColor(color) {
+        newBoxList[i].style.backgroundColor = color;
+     }
+    newBoxList[i].addEventListener('mouseover', changeBoxColor('red'));
+}
+
+/* 
+ok, so now every now is red. what does this mean?
+this means that we have figured out:
+    1. how to iterate over the NodeList 
+    2. and change the color as we go
+but we lost our nice hover effect...
+that we did
+*/
