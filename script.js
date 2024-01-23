be14240ba5af9200f86ec054afd19eb78944f2dc
rfloarea <account@rfloarea.net>
2024-01-23 15:31:38 -0500

    feat: click to change box color

--- script.js
+++ script.js
@@ -15,43 +15,13 @@ function appendBox(i) {
 
 appendBox();
 
-
-
 const newBoxList = document.querySelectorAll('#box');
-console.log(newBoxList); // this logs a NodeList of all 256 elements
 
-/* newBox is a NodeList (an array type object) of all elements with id = box
-ok...so now what?
-now we need to find a way to attach event listeners to each node on this list
-is that possible?
-let's see
-*/
+changeBoxColor();
 
-for (let i = 0; i < newBoxList.length; i++) {
-    function changeBoxColor(color) {
-        newBoxList[i].style.backgroundColor = color;
-     }
-    newBoxList[i].addEventListener('mouseover', changeBoxColor('red'));
-}
 
-/* 
-ok, so now every now is red. what does this mean?
-this means that we have figured out:
-    1. how to iterate over the NodeList 
-    2. and change the color as we go
-but we lost our nice hover effect...
-that we did
-
-let's back up a second to see if we can add one lick event to one element
-*/
-
-let oneBox = document.getElementById('box');
-console.log(oneBox);
-oneBox.addEventListener('click', alert('this works!'));
-/* huh, see what happened? the event triggered without clicking
-that's weird!
-well, yes and no
-it's weird because you are a baby and don't know anything
-it's not weird because if you knew that (insert logical explanation here => something about 
-    needing to use a callback rather than directly calling the function as the handler)
-*/
+function changeBoxColor() {
+    for (let i = 0; i < newBoxList.length; i++) {
+        newBoxList[i].addEventListener('click', () => newBoxList[i].style.backgroundColor = 'red');
+    }
+ }
