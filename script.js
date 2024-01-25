b13129d09d22f235773ad51c37e2894e41e14493
rfloarea <account@rfloarea.net>
2024-01-24 20:29:42 -0500

    honestly, ive lost track: - settign class and ids - plotting out the story of these functions - hide/show modal functions - overlay hide/show functions

--- script.js
+++ script.js
@@ -1,17 +1,75 @@
-const canvasFrame = document.getElementById('canvasFrame');
+window.addEventListener('load', startUp);
+
+function startUp() {
+    startUpModal();
+    drawCanvas();
+    drawBoxes();
+    addEventListeners();
+    setDefaultColor();
+  };
+  
+function startUpModal() {
+    showModal();
+    getCanvasSize();
+    getBoxSize();
+    getStartingColor();
+    hideModal();
+};
+
+// modal stuff
+const modal = document.querySelector('#modal');
+function showModal() {
+    modal.classList.add('active');
+}
+function hideModal() {
+    modal.classList.remove('active');
+}
+
+// overlay stuff
+const overlay = document.querySelector('#overlay');
+function showOverlay() {
+    overlay.classList.add('active');
+}
+function hideOverlay() {
+    overlay.classList.remove('active');
+}
+
+//get canvas size
+//const canvasSize = document.querySelector('#canvas');
+const canvasSize = document.querySelector('#canvasDropdown').value;
+console.log(canvasSize);
+
+function getCanvasSize() {
+    switch (canvasSize) {
+        case "small":
+            canvas.classList.add('sml-Canvas')
+            break;
+        case "medium":
+            canvas.classList.add('med-canvas')
+            break;
+        case "large":
+            canvasSize.classList.add('lrg-canvas')
+            break;
+    };
+};
+  
+
+
+// functioning shit is below  
+const canvasFrame = document.getElementById('canvas');
 const box = document.createElement('div')
-const numberOfBoxes = 256; //prompt('How many boxes would you like in your canvas? Max is 256.');
+const numberOfBoxes = 256;
 const arr = Array.from('x'.repeat(numberOfBoxes));
 
 appendBox();
 
 function appendBox(i) {
-    for (i = 0; i < arr.length; i++ ){
-        const newElement  = document.createElement('div');
-        newElement.classList.add('box');
-        newElement.setAttribute('id', 'box');
-        canvasFrame.appendChild(newElement);
-    }
+for (i = 0; i < arr.length; i++ ){
+    const newElement  = document.createElement('div');
+    newElement.classList.add('box');
+    newElement.setAttribute('id', 'box');
+    canvasFrame.appendChild(newElement);
+}
 }
 
 const newBoxList = document.querySelectorAll('#box');
@@ -19,7 +77,7 @@ const newBoxList = document.querySelectorAll('#box');
 changeBoxColor();
 
 function changeBoxColor() {
-    for (let i = 0; i < newBoxList.length; i++) {
-        newBoxList[i].addEventListener('click', () => newBoxList[i].style.backgroundColor = 'red');
-    }
- }
+for (let i = 0; i < newBoxList.length; i++) {
+    newBoxList[i].addEventListener('click', () => newBoxList[i].style.backgroundColor = 'red');
+}
+}
